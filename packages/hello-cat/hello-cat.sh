367808b2f411405dfe1dd3b3ddb545674066c8f1
notmeower <94183274+notmeower77463955@users.noreply.github.com>
2025-08-06 21:59:34 +0200

    Update hello-cat.sh

--- packages/hello-cat/hello-cat.sh
+++ packages/hello-cat/hello-cat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo '#!/bin/bash' > /usr/local/bin/hello-cat
-echo 'echo Hello from hello-cat!' >> /usr/local/bin/hello-cat
+echo 'echo hi twin' >> /usr/local/bin/hello-cat
 
 chmod +x /usr/local/bin/hello-cat
-echo "* Installed hello-cat into user binary directory."
+echo "* Module installed successfully."
